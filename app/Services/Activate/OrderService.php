82b0b3829a3cdfafc2ef8f316bc26df449e93159
evans86 <evans86@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>
2023-07-21 00:48:14 +0300

    add all logs

--- app/Services/Activate/OrderService.php
+++ app/Services/Activate/OrderService.php
@@ -157,7 +157,7 @@ class OrderService extends MainService
             $user->service,
             $country_id
         );
-        $org_id = intval($serviceResult['actitionId']);
+        $org_id = intval($serviceResult['activationId']);
         // Из него получить цену
         $amountStart = intval(floatval($serviceResult['activationCost']) * 100);
         $amountFinal = $amountStart + $amountStart * $botDto->percent / 100;
