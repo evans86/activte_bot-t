ce9a010cfe230397b8a5282929b79b8cba4ef0f3
evans <evandaniil85@gmail.com>
2024-12-06 23:51:25 +0300

    fix services cache

--- app/Services/Activate/BotService.php
+++ app/Services/Activate/BotService.php
@@ -9,7 +9,8 @@ use App\Services\MainService;
 
 class BotService extends MainService
 {
-    const DEFAULT_HOST = 'https://api.sms-activate.org/stubs/handler_api.php';
+//    const DEFAULT_HOST = 'https://api.sms-activate.org/stubs/handler_api.php';
+    const DEFAULT_HOST = 'https://api.sms-activate.ae/stubs/handler_api.php';
 
     /**
      * Создание модуля
