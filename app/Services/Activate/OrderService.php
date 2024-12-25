5667addc9f936d0a4c66efe0d1730201266c91d2
evans <evandaniil85@gmail.com>
2024-12-25 22:25:42 +0300

    fix services cache

--- app/Services/Activate/OrderService.php
+++ app/Services/Activate/OrderService.php
@@ -262,11 +262,11 @@ class OrderService extends MainService
      */
     public function updateStatusCancel($order_id): void
     {
-        \DB::transaction(function () use ($order_id) {
+//        \DB::transaction(function () use ($order_id) {
             $order = SmsOrder::lockForUpdate()->where(['org_id' => $order_id])->where(['status' => SmsOrder::STATUS_WAIT_CODE])->first();
             $order->status = SmsOrder::STATUS_CANCEL;
             $order->save();
-        });
+//        });
     }
 
     /**
